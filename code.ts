933476282f2843915148c4938e29f7250d949e39
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-19 18:16:07 +0200

    get and format CSS for selection

--- code.ts
+++ code.ts
@@ -1,18 +1,25 @@
-// const node = figma.currentPage.selection[0];
+if (figma.editorType === "dev" && figma.mode === "codegen") {
+  figma.codegen.on("generate", async () => {
+    const node = figma.currentPage.selection[0];
+    let css = {};
+    if (node) {
+      css = await node.getCSSAsync();
+    } else {
+      figma.notify("Select something");
+    }
 
-if (figma.editorType === "dev") {
-  // Read the current page and listen to API events
-  const numChildren = figma.currentPage.children.length;
-  figma.notify(
-    `This is running in Dev Mode.
-    The current page has ${numChildren}`
-  );
-} else {
-  figma.notify(
-    `This is NOT running in Dev Mode.
-    We can modify the file!`
-  );
-  const node = figma.createRectangle();
-  node.name = "I proved that I can edit files!";
+    let rules = [];
+    for (const key of Object.keys(css) as Array<keyof typeof css>) {
+      const value = css[key];
+      rules.push(`${key}: ${value};`);
+    }
+
+    return [
+      {
+        language: "CSS",
+        code: rules.join("\n"),
+        title: "My custom CSS",
+      },
+    ];
+  });
 }
-figma.closePlugin();
