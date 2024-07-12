90e42ec5390ea9ed578361f7e12552e243b5824b
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-12 16:05:59 +0200

    refactor: navbar styles

--- ui/src/Navbar.tsx
+++ ui/src/Navbar.tsx
@@ -6,9 +6,9 @@ interface NavbarProps {
 
 export function Navbar({ copyRef }: NavbarProps) {
   return (
-    <div className="navbar">
-      <h1 className="title">tailwind.config.js</h1>
-      <button className="copy-button" ref={copyRef}>
+    <div className="p-1 flex justify-between items-center">
+      <h1 className="title ps-3 text-base font-normal">tailwind.config.js</h1>
+      <button className="copy-button p-2 bg-transparent" ref={copyRef}>
         <ClipboardCopy size={20} strokeWidth={1.50} />
       </button>
     </div>
