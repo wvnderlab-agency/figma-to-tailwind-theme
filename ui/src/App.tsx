c5f6abf3fd83b8a6df925ba703c10af03e34481b
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-12 13:56:11 +0200

    refactor: add Navbar component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { Navbar } from "./Navbar";
 
 function App() {
   const configRef = useRef<HTMLTextAreaElement | null>(null);
@@ -29,29 +30,7 @@ function App() {
 
   return (
     <main className="container">
-      <div className="navbar">
-        <h1 className="title">tailwind.config.js</h1>
-        <button className="copy-button" ref={copyRef}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="1.25"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-clipboard-copy"
-          >
-            <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
-            <path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2" />
-            <path d="M16 4h2a2 2 0 0 1 2 2v4" />
-            <path d="M21 14H11" />
-            <path d="m15 10-4 4 4 4" />
-          </svg>
-        </button>
-      </div>
+      <Navbar copyRef={copyRef} />
       <textarea
         readOnly
         ref={configRef}
