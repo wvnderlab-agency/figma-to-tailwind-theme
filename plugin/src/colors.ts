2cd2b051fc0379ca70ec6131cfa0b1cbeb7f7ae6
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-17 18:10:17 +0200

    print error messages when colors fail to load

--- plugin/src/colors.ts
+++ plugin/src/colors.ts
@@ -1,3 +1,4 @@
+import { PLUGIN_NAME } from "./settings";
 import { toClassName, rgbToString } from "./utils";
 
 interface ColorModes {
@@ -10,50 +11,56 @@ export interface Colors {
 
 export async function getColorsFromVars() {
   const colors: Colors = {};
-  const collections = await figma.variables.getLocalVariableCollectionsAsync();
 
-  for (const collection of collections) {
-    const hasSingleMode = collection.modes.length === 1;
+  try {
+    const collections =
+      await figma.variables.getLocalVariableCollectionsAsync();
 
-    if (hasSingleMode) {
-      for (const varId of collection.variableIds) {
-        let variable = await figma.variables.getVariableByIdAsync(varId);
-        if (!variable || variable.resolvedType !== "COLOR") continue;
+    for (const collection of collections) {
+      const hasSingleMode = collection.modes.length === 1;
 
-        let colorName = toClassName(variable.name);
-        let colorValue = rgbToString(
-          variable.valuesByMode[collection.defaultModeId] as RGB | RGBA,
-        );
+      if (hasSingleMode) {
+        for (const varId of collection.variableIds) {
+          let variable = await figma.variables.getVariableByIdAsync(varId);
+          if (!variable || variable.resolvedType !== "COLOR") continue;
 
-        colors[colorName] = colorValue;
-      }
-    } else {
-      for (const varId of collection.variableIds) {
-        let variable = await figma.variables.getVariableByIdAsync(varId);
-        if (!variable || variable.resolvedType !== "COLOR") continue;
-
-        let colorName = toClassName(variable?.name || "unknown");
-
-        for (const mode of collection.modes) {
+          let colorName = toClassName(variable.name);
           let colorValue = rgbToString(
-            variable.valuesByMode[mode.modeId] as RGB | RGBA,
+            variable.valuesByMode[collection.defaultModeId] as RGB | RGBA,
           );
-          let colorMode = toClassName(mode.name);
+
+          colors[colorName] = colorValue;
+        }
+      } else {
+        for (const varId of collection.variableIds) {
+          let variable = await figma.variables.getVariableByIdAsync(varId);
+          if (!variable || variable.resolvedType !== "COLOR") continue;
+
+          let colorName = toClassName(variable?.name || "unknown");
+
+          for (const mode of collection.modes) {
+            let colorValue = rgbToString(
+              variable.valuesByMode[mode.modeId] as RGB | RGBA,
+            );
+            let colorMode = toClassName(mode.name);
+            colors[colorName] = {
+              ...(colors[colorName] as ColorModes),
+              [colorMode]: colorValue,
+            };
+          }
+
+          let colorValue = rgbToString(
+            variable.valuesByMode[collection.defaultModeId] as RGB | RGBA,
+          );
           colors[colorName] = {
             ...(colors[colorName] as ColorModes),
-            [colorMode]: colorValue,
+            DEFAULT: colorValue,
           };
         }
-
-        let colorValue = rgbToString(
-          variable.valuesByMode[collection.defaultModeId] as RGB | RGBA,
-        );
-        colors[colorName] = {
-          ...(colors[colorName] as ColorModes),
-          DEFAULT: colorValue,
-        };
       }
     }
+  } catch (err) {
+    console.error(`Fetching color variables failed from ${PLUGIN_NAME}`, err);
   }
 
   return colors;
@@ -61,15 +68,20 @@ export async function getColorsFromVars() {
 
 export async function getColorsFromStyles() {
   const colors: Colors = {};
-  const paintStyles = await figma.getLocalPaintStylesAsync();
 
-  for (let style of paintStyles) {
-    if (style.paints.length !== 1 || style.paints[0]?.type !== "SOLID")
-      continue;
+  try {
+    const paintStyles = await figma.getLocalPaintStylesAsync();
 
-    let colorName = toClassName(style.name);
-    let colorValue = rgbToString((style.paints[0] as SolidPaint).color);
-    colors[colorName] = colorValue;
+    for (let style of paintStyles) {
+      if (style.paints.length !== 1 || style.paints[0]?.type !== "SOLID")
+        continue;
+
+      let colorName = toClassName(style.name);
+      let colorValue = rgbToString((style.paints[0] as SolidPaint).color);
+      colors[colorName] = colorValue;
+    }
+  } catch (err) {
+    console.error(`Fetching color styles failed from ${PLUGIN_NAME}:`, err);
   }
 
   return colors;
