85dfb2856c8a966676a0b0f4bc19b5d19c94fbf8
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-18 12:35:24 +0200

    enable plugin in inspect panel

--- plugin/src/code.ts
+++ plugin/src/code.ts
@@ -1,6 +1,26 @@
 import { getTwConfigStr } from "./tw-config";
 
 if (figma.editorType === "figma") {
+  setupUI();
+} else if (figma.editorType === "dev") {
+  if (figma.mode === "codegen") {
+    figma.codegen.on("generate", async () => {
+      const code = await getTwConfigStr();
+
+      return [
+        {
+          language: "JAVASCRIPT",
+          code,
+          title: "tailwind.config.js",
+        },
+      ];
+    });
+  } else if (figma.mode === "inspect") {
+    setupUI();
+  }
+}
+
+function setupUI() {
   figma.showUI(__html__, { themeColors: true, width: 800, height: 600 });
 
   figma.ui.onmessage = async (message) => {
@@ -12,16 +32,4 @@ if (figma.editorType === "figma") {
       figma.notify("Copied to clipboard");
     }
   };
-} else if (figma.editorType === "dev" && figma.mode === "codegen") {
-  figma.codegen.on("generate", async () => {
-    const code = await getTwConfigStr();
-
-    return [
-      {
-        language: "JAVASCRIPT",
-        code,
-        title: "tailwind.config.js",
-      },
-    ];
-  });
 }
