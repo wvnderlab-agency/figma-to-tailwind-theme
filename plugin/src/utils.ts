61bf44ba8b7a892ff89f4cb06f736a301a729f43
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-11 15:39:28 +0200

    fix:rgba values contain double 0

--- plugin/src/utils.ts
+++ plugin/src/utils.ts
@@ -16,13 +16,10 @@ export function toClassName(str: string) {
 export function rgbToString(color: RGB | RGBA) {
   const red = Math.round(color.r * 255)
     .toString()
-    .padStart(2, "0");
   const green = Math.round(color.g * 255)
     .toString()
-    .padStart(2, "0");
   const blue = Math.round(color.b * 255)
     .toString()
-    .padStart(2, "0");
   const alpha = color.hasOwnProperty("a") ? (color as RGBA).a : 1;
 
   return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
